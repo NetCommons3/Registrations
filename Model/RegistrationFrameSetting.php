a86e0567c417df8d594094f5ce6fb1c51aeb29de
RyujiAMANO <ryuji@ryus.co.jp>
2016-06-24 10:59:26 +0900

    フレーム設定の不要なバリデートをコメントアウト

--- Model/RegistrationFrameSetting.php
+++ Model/RegistrationFrameSetting.php
@@ -75,22 +75,22 @@ class RegistrationFrameSetting extends RegistrationsAppModel {
 					'message' => __d('net_commons', 'Invalid request.'),
 				),
 			),
-			'display_num_per_page' => array(
-				'number' => array(
-					'rule' => array('numeric'),
-					'message' => __d('net_commons', 'Invalid request.'),
-				),
-				'inList' => array(
-					'rule' => array('inList', array(1, 5, 10, 20, 50, 100)),
-					'message' => __d('net_commons', 'Invalid request.'),
-				),
-			),
-			'sort_type' => array(
-				'inList' => array(
-					'rule' => array('inList', array_keys(RegistrationsComponent::getSortOrders())),
-					'message' => __d('net_commons', 'Invalid request.'),
-				),
-			),
+			//'display_num_per_page' => array(
+			//	'number' => array(
+			//		'rule' => array('numeric'),
+			//		'message' => __d('net_commons', 'Invalid request.'),
+			//	),
+			//	'inList' => array(
+			//		'rule' => array('inList', array(1, 5, 10, 20, 50, 100)),
+			//		'message' => __d('net_commons', 'Invalid request.'),
+			//	),
+			//),
+			//'sort_type' => array(
+			//	'inList' => array(
+			//		'rule' => array('inList', array_keys(RegistrationsComponent::getSortOrders())),
+			//		'message' => __d('net_commons', 'Invalid request.'),
+			//	),
+			//),
 		));
 
 		parent::beforeValidate($options);
