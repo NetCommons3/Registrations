7497b637e7568dd6625b21a2720e4a7a59c21227
RyujiAMANO <ryuji@ryus.co.jp>
2017-10-08 17:25:09 +0900

    受付番号のマイグレーション。すでに登録されたデータの受付番号はRegistrationAnswerSummary.idとした。 これまで登録完了時の画面上に「受け付けID」として表示されてたため。

---
+++ Config/Migration/1507450204_add_answer_summary_serial_number.php
@@ -0,0 +1,68 @@
+<?php
+class AddAnswerSummarySerialNumber extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'add_answer_summary_serial_number';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'registration_answer_summaries' => array(
+					'serial_number' => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => true, 'after' => 'id'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_field' => array(
+				'registration_answer_summaries' => array('serial_number'),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		$this->RegistrationAnswerSummary = $this->generateModel('RegistrationAnswerSummary');
+
+		if ($direction == 'up') {
+			// 受付番号をIDで埋める。
+			$summaries = $this->RegistrationAnswerSummary->find('all', [
+				'conditions' => [
+					'answer_status' => 2
+				]
+			]);
+			foreach ($summaries as $summary) {
+				$summary['RegistrationAnswerSummary']['serial_number'] = $summary['RegistrationAnswerSummary']['id'];
+				$this->RegistrationAnswerSummary->create();
+				$this->RegistrationAnswerSummary->save($summary, [
+					'validate' => false,
+					'callbacks' => false
+				]);
+			}
+		}
+		return true;
+	}
+}
