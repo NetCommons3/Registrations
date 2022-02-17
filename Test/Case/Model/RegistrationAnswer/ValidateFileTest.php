b2d5f0116b17e59c2e1203a38f17cfd452f8c5e6
Ryuji AMANO <ryuji@ryus.co.jp>
2022-02-17 14:06:33 +0900

    fix: ファイルに対してファイルタイプのバリデーションが有効にならなかったのを修正 Refs https://github.com/researchmap/RmNetCommons3/issues/2659

--- Test/Case/Model/RegistrationAnswer/ValidateFileTest.php
+++ Test/Case/Model/RegistrationAnswer/ValidateFileTest.php
@@ -12,16 +12,32 @@
 
 App::uses('NetCommonsModelTestCase', 'NetCommons.TestSuite');
 
+/**
+ * RegistrationsValidateFileTest
+ */
 final class RegistrationsValidateFileTest extends \NetCommonsModelTestCase {
 
+/**
+ * @var array fixtures プロパティなしだとNC3の各種Fixtureがセットされないので空でも宣言しておく。
+ */
 	public $fixtures = [];
 
+/**
+ * setUp
+ *
+ * @return void
+ */
 	public function setUp() {
 		parent::setUp();
 		// 常に新規インスタンスでテストしたいので最初に生成済みインスタンスを削除しておく。
 		ClassRegistry::removeObject('RegistrationAnswer');
 	}
 
+/**
+ * removeObject後に再度initすれば新しいインスタンスを取得できることを確認
+ *
+ * @return void
+ */
 	public function testRemoveObject() {
 		$answerModel = ClassRegistry::init('Registrations.RegistrationAnswer');
 		ClassRegistry::removeObject('RegistrationAnswer');
@@ -29,6 +45,11 @@ final class RegistrationsValidateFileTest extends \NetCommonsModelTestCase {
 		self::assertNotSame($answerModel, $answerModel2);
 	}
 
+/**
+ * ファイル型でなければファイルのバリデーションはセットされない
+ *
+ * @return void
+ */
 	public function testNoFileValidationIfQuestionTypeNotFile() {
 		/** @var RegistrationAnswer $answerModel */
 		$answerModel = ClassRegistry::init('Registrations.RegistrationAnswer');
@@ -44,6 +65,11 @@ final class RegistrationsValidateFileTest extends \NetCommonsModelTestCase {
 		$this->assertArrayNotHasKey('answer_value_file', $answerModel->validate);
 	}
 
+/**
+ * ファイル型だとファイルのバリデーションがセットされる。
+ *
+ * @return void
+ */
 	public function testExistsFileValidationIfQuestionTypeIsFile() {
 		/** @var RegistrationAnswer $answerModel */
 		$answerModel = ClassRegistry::init('Registrations.RegistrationAnswer');
@@ -60,6 +86,11 @@ final class RegistrationsValidateFileTest extends \NetCommonsModelTestCase {
 		$this->assertArrayHasKey('answer_value_file', $answerModel->validate);
 	}
 
+/**
+ * テキスト型をバリデーションの次にファイル型をバリデーションするときもファイル型バリデーションがある
+ *
+ * @return void
+ */
 	public function testExistsFileValidationIfAfterNotFileType() {
 		/** @var RegistrationAnswer $answerModel */
 		$answerModel = ClassRegistry::init('Registrations.RegistrationAnswer');
@@ -87,4 +118,38 @@ final class RegistrationsValidateFileTest extends \NetCommonsModelTestCase {
 		$answerModel->beforeValidate($options);
 		$this->assertArrayHasKey('answer_value_file', $answerModel->validate);
 	}
+
+/**
+ * ファイル型をバリデーションの次にテキスト型をバリデーションするときにファイル型バリデーションがないこと
+ *
+ * @return void
+ */
+	public function testNotExistsFileValidationIfAfterFileType() {
+		/** @var RegistrationAnswer $answerModel */
+		$answerModel = ClassRegistry::init('Registrations.RegistrationAnswer');
+		// ファイル型の回答バリデーション組立
+		$options = [
+			'registration_answer_summary_id' => '1',
+			'allAnswers' => null,
+			'question' => [
+				'question_type' => RegistrationsComponent::TYPE_FILE,
+				'is_require' => false,
+				'question_value' => null,
+			]
+		];
+		$answerModel->beforeValidate($options);
+
+		// テキスト型の回答バリデーション組立
+		$options = [
+			'registration_answer_summary_id' => '1',
+			'allAnswers' => null,
+			'question' => [
+				'question_type' => RegistrationsComponent::TYPE_TEXT,
+				'question_value' => null,
+			]
+		];
+		$answerModel->beforeValidate($options);
+		$this->assertArrayNotHasKey('answer_value_file', $answerModel->validate);
+	}
+
 }
